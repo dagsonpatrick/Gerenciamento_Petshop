986060d584f79e9106a7c66b10374ec229abbee3
dagsopatrick <dagsonmg@yahoo.com.br>
2022-02-17 13:44:37 -0300

    removido credenciais do email

--- gerenciamento_pet/settings.py
+++ gerenciamento_pet/settings.py
@@ -79,7 +79,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'dagsonmg@gmail.com'
-EMAIL_HOST_PASSWORD = 'tuahiwwjnbkiavcm'
+EMAIL_HOST_PASSWORD = ''
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
